524c16dfc904999afeafde882df43acf3beda5ab
Matthew <Matirix@live.com>
2024-05-04 12:32:43 -0700

    Adding existing library

---
+++ backend/helper.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+export function validateBookFields(req, res, next) {
+    if (
+        !req.body.title ||
+        !req.body.author ||
+        !req.body.publishYear
+    ) {
+        return res.status(400).send({ message: "All fields are required" });
+    }
+    next();
+}
